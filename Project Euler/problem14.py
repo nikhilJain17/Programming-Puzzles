16d4f7d4cfdec24f525fd499f0e39e0da9a7bce7
nikhilJain17 <nikhil.jain0987@gmail.com>
2017-07-03 12:39:44 -0400

    finished problem 14

---
+++ Project Euler/problem14.py
@@ -0,0 +1,49 @@
+"""""The following iterative sequence is defined for the set of positive integers:
+
+n -> n/2 (n is even)
+n -> 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+
+"""""
+import numpy
+
+# counts function calls
+counter = 0
+def collatz_length(num):
+	
+	global counter
+	if num == 1:
+		return 777 # junk
+
+	elif num % 2 == 0:	
+		counter += 1
+		return collatz_length(num/2)
+	else:
+		counter += 1
+		return collatz_length(3 * num + 1)
+
+
+def main():
+
+	res = []
+
+	for x in range(1, 1000001):
+		global counter 
+
+		collatz_length(x)
+		res.append(counter)
+		print str(x) + ', ' + str(counter)
+
+		counter = 0
+
+	print "Answer is: " + str(numpy.argmax(res) + 1)
+
+main()
