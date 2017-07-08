60f303dd0650f5b6f92e7aa68402fa7d040cc2db
nikhilJain17 <nikhil.jain0987@gmail.com>
2017-07-08 13:15:38 -0400

    finished problem

--- LeetCode/powOf3noLoops.py
+++ LeetCode/powOf3noLoops.py
@@ -4,50 +4,16 @@ Follow up:
 Could you do it without using any loop / recursion?
 
 """""
+import math
 
 # Nikhil Jain
 
-# powers of three have the following sequence for last digit:
-# 3, 9, 7, 1
-# this program uses that fact to figure out the answer
-# could be shorter but is more thorough in this form
-
+# just use logs
+# log func either returns above or below int value if true
+# i.e. 3^5 = 243
+# but math.log(243, 3) == 4.9999999999 or 5.0000000001
+# so handle both
 def isPowerOfThree(n):
-
-	digitsList = [3, 9, 7, 1]
-	lastDigit = n % 10
+	if n <= 0: return False
+	return math.fabs(math.log(n, 3) - math.floor(math.log(n,3))) == 0 or math.fabs(math.log(n, 3) - math.floor(math.log(n,3)) - 1) < 0.0000000001 
 	
-	if n <= 0:
-		return False
-		
-
-	if lastDigit not in digitsList:
-		return False
-	
-	nDigsList = []
-	nDigsList.append((n * 3) % 10)
-	nDigsList.append((n * 9) % 10)
-	nDigsList.append((n * 27) % 10)
-	nDigsList.append((n * 81) % 10)
-
-	print nDigsList
-
-	# rotate nDigsList for thoroughness
-	# so if n is pow of 3, nDigsList and digitsList are identical
-	# although i don't think this is necessary
-	# order doesn't really matter but just to be thorough
-	
-	if nDigsList.count(3) is 0:
-		# safety check for rotating about 3
-		# because if 3 isn't in nDigsList, func index throws err
-		return false
-
-	start = nDigsList.index(3)
-	nDigsList = nDigsList[start:] + nDigsList[:start]
-
-	print nDigsList
-
-	if nDigsList == digitsList:
-		return True
-	else:
-		return False
